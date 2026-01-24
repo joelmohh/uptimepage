8345b38ec45a3736d8af69abaff8bf07b1084ad7
Joel Moh <heitor.juliani19@gmail.com>
2026-01-23 20:17:39 -0700

    version 0.0.1 - semi done ui

---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const currentTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    if (currentTheme === "dark") {
+        themeToggleBtn.innerHTML = '<img src="https://raw.githubusercontent.com/joelmohh/joelmohh/refs/heads/main/img/moon.svg" alt="">';
+    } else {
+        themeToggleBtn.innerHTML = '<img src="https://raw.githubusercontent.com/joelmohh/joelmohh/refs/heads/main/img/sun.svg" alt="">';
+    }
+}
+
+themeToggleBtn.addEventListener('click', function () {
+    let theme = document.documentElement.getAttribute("data-theme");
+    if (theme === "light") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+        themeToggleBtn.innerHTML = '<img src="https://raw.githubusercontent.com/joelmohh/joelmohh/refs/heads/main/img/moon.svg" alt="">';
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+        themeToggleBtn.innerHTML = '<img src="https://raw.githubusercontent.com/joelmohh/joelmohh/refs/heads/main/img/sun.svg" alt="">';
+    }
+});
+
+// Uptime bars
+document.querySelectorAll('.service-card').forEach(card => {
+    const name = card.getAttribute('data-service');
+    const descprition = card.getAttribute('data-description') || 'No description available.';
+    card.innerHTML = `
+                <div class="service-header">
+                    <h3>${name}</h3>
+                    <span class="badge online">Operational</span>
+                </div>
+                <p class="service-desc">${descprition}</p>
+                <div class="uptime-wrapper">
+                    <div class="uptime-bar"></div>
+                    <div class="uptime-footer">
+                        <span>90 days ago</span>
+                        <span>100% uptime</span>
+                        <span>Today</span>
+                    </div>
+                </div>
+            `;
+
+    const barContainer = card.querySelector('.uptime-bar');
+    for (let i = 0; i < 100; i++) {
+        const segment = document.createElement('span');
+        segment.className = 'segment';
+        barContainer.appendChild(segment);
+    }
+});
